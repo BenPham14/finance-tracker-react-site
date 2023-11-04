019a0630aacc84f93663991302d467166292a8db
Benjamin Pham <97713069+BenPham14@users.noreply.github.com>
2023-11-04 10:54:11 -0700

    Finish hero section and add mobile menu

--- finance-tracker/src/pages/home/index.js
+++ finance-tracker/src/pages/home/index.js
@@ -1,27 +1,44 @@
+import { useState } from 'react';
 import hero from '../../assets/undraw_investor_update_re_qnuu.svg';
 import homeCSS from './home.module.css';
+import { HiMenuAlt3 } from 'react-icons/hi'
+
+const DropDown = ({toggle}) => {
+    if (!toggle) return;
+    return (
+        <nav className={homeCSS.dropDown}>
+            <p>Features</p>
+            <p>FAQ</p>
+            <button>Login</button>
+        </nav>
+    );
+};
 
 const Home = () => {
-    return(
+    const [toggle, setToggle] = useState(false);
+
+    return (
         <main className={homeCSS.home}>
             <header>
-                <p>FinTracker</p>
-                <div>
-                    <p>Features</p>
-                    <p>FAQ</p>
+                <h2>FinTracker</h2>
+                <HiMenuAlt3 className={homeCSS.menuIcon} onClick={() => setToggle(!toggle)}/>
+                <nav className={homeCSS.rightLinks}>
+                    <a>Features</a>
+                    <a>FAQ</a>
                     <button>Login</button>
-                </div>
+                </nav>
+                <DropDown toggle={toggle}/>
             </header>
             <section className={homeCSS.hero}>
-                <div>
-                    <h1>Finances Made Easy</h1>
-                    <p>
-                        Track your expenses effortlessly and save more money with FinTracker, 
-                        the smart and simple app that helps you manage your finances.
-                    </p>
-                    <button>Login</button>
-                </div>
-                <img src={hero} alt="hero"></img>
+                    <div>
+                        <h1>Finances Made Easy</h1>
+                        <p>
+                            Track your expenses effortlessly and save more money with FinTracker, 
+                            the smart and simple app that helps you manage your finances.
+                        </p>
+                        <button>Login</button>
+                    </div>
+                    <img src={hero} alt="hero"/>
             </section>
             <section className={homeCSS.features}>
                 <div>
