ef6c8b8f08ed47b4126f3b6d69acc0dd32ea4d62
Benjamin Pham <97713069+BenPham14@users.noreply.github.com>
2023-12-07 22:14:00 -0800

    Create carousel for new user screen

--- finance-tracker/src/pages/main/NewUser.js
+++ finance-tracker/src/pages/main/NewUser.js
@@ -1,19 +1,67 @@
-const NewUser = () => {
+import mainCSS from './main.module.css';
+import hero from '../../assets/undraw_investor_update_re_qnuu.svg';
+import { useState } from 'react';
+
+const CarouselItem = ({id, title, image, text, custom, currentStep}) => {
     return (
-        <main>
-            <p>Welcome</p>
-            <ul>
-                <li>Get Started</li>
-                <li>Create Account</li>
-                <li>Create Budget</li>
-                <li>All Set</li>
-            </ul>
-            <button>
-                Previous
-            </button>
-            <button>
-                Next
-            </button>
+        <div className={mainCSS.carouselItem} style={{display: currentStep !== id && 'none'}}>
+            <h2>{title}</h2>
+            <img src={image} alt={title}/>
+            {custom}
+            <p>{text}</p>
+        </div>
+    );
+};
+
+const NewUser = () => {
+    const items = [
+        {id: 1, title: 'Get Started', image: hero, text: 'text', custom: ''},
+        {id: 2, title: 'Create Account', image: hero, text: 'text', custom: <input/>},
+        {id: 3, title: 'Create Budget', image: hero, text: 'text', custom: <input/>},
+        {id: 4, title: 'All Set', image: hero, text: 'text', custom: ''},
+    ];
+
+    const [currentStep, setCurrentStep] = useState(1);
+
+    const backCurrentStep = () => {
+        if (currentStep > 1) {
+            setCurrentStep(currentStep - 1);
+        };
+    };
+
+    const nextCurrentStep = () => {
+        if (currentStep < 4) {
+            setCurrentStep(currentStep + 1);
+        };
+    };
+
+    return (
+        <main className={mainCSS.newUser}>
+            <h1>Welcome</h1>
+            <div className={mainCSS.steps}>
+                <img src='' alt=''/>
+                <img src='' alt=''/>
+                <img src='' alt=''/>
+                <img src='' alt=''/>
+            </div>
+            <div className={mainCSS.carousel}>
+                {
+                    items.map((item) => (
+                        <CarouselItem key={item.id}
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            text={item.text}
+                            custom={item.custom}
+                            currentStep={currentStep}
+                        />
+                    ))
+                }
+            </div>
+            <div className={mainCSS.carouselNav}>
+                <button onClick={backCurrentStep}>Back</button>
+                <button onClick={nextCurrentStep}>Next</button>
+            </div>
         </main>
     );
 };
