66b28efa50d15b4b73f9a421a3facd8fa475c140
Benjamin Pham <97713069+BenPham14@users.noreply.github.com>
2024-05-24 16:49:52 -0700

    Seperate delete transaction feature to new component

---
+++ finance-tracker/src/components/table/DeleteTransaction.js
@@ -0,0 +1,29 @@
+import deleteCSS from '../../components/delete/delete.module.css';
+import { deleteDoc, doc } from 'firebase/firestore';
+import { db } from '../../config/firebase';
+
+const DeleteTransaction = ({data, setIsOpen, setDeleteOpen}) => {
+    const cancelDelete = (e) => {
+        e.preventDefault();
+        setDeleteOpen(false);
+    };
+
+    const deleteTranscation = async (e, docId) => {
+        e.preventDefault();
+        let docRef = doc(db, "transactions", docId);
+        await deleteDoc(docRef);
+        setIsOpen(false);
+    };
+
+    return (
+        <>
+            <p>Are you sure you want to delete: {data.name} ({data.type === 'expense' && '-'}${data.amount})?</p>
+            <div className={deleteCSS.deleteCancelButtons}>
+                <button id={deleteCSS.cancel} onClick={cancelDelete}>Cancel</button>
+                <button id={deleteCSS.delete} onClick={(e) => deleteTranscation(e, data.docId)}>Delete</button>
+            </div>
+        </> 
+    );
+};
+
+export default DeleteTransaction;
--- finance-tracker/src/components/table/EditTransaction.js
+++ finance-tracker/src/components/table/EditTransaction.js
@@ -1,9 +1,9 @@
 import tableCSS from './table.module.css';
-import deleteCSS from '../../components/delete/delete.module.css';
-import { deleteDoc, updateDoc, doc } from 'firebase/firestore';
+import { updateDoc, doc } from 'firebase/firestore';
 import { db } from '../../config/firebase';
 import { categories } from '../../context/context';
 import { useEffect, useState } from 'react';
+import DeleteTransaction from './DeleteTransaction';
 
 const EditTransaction = ({data, accounts, isOpen, setIsOpen, deleteOpen, setDeleteOpen}) => {
     const [form, setForm] = useState({
@@ -47,21 +47,9 @@ const EditTransaction = ({data, accounts, isOpen, setIsOpen, deleteOpen, setDele
         setIsOpen(false);
     }
 
-    const deleteTranscation = async (e, docId) => {
-        e.preventDefault();
-        let docRef = doc(db, "transactions", docId);
-        await deleteDoc(docRef);
-        setIsOpen(false);
-    };
-
     const openDelete = (e) => {
         e.preventDefault();
         setDeleteOpen(true);
-    };
-
-    const cancelDelete = (e) => {
-        e.preventDefault();
-        setDeleteOpen(false);
     };
 
     const changePlaceholderColor = (value) => {
@@ -140,20 +128,18 @@ const EditTransaction = ({data, accounts, isOpen, setIsOpen, deleteOpen, setDele
                         </select>
                     </div>
                     <div className={tableCSS.transactionBtns}>
-                        <button id={deleteCSS.delete} onClick={openDelete}>Delete</button>
+                        <button id={tableCSS.delete} onClick={openDelete}>Delete</button>
                         <div>
                             <button id={tableCSS.cancel} onClick={closeEdit}>Cancel</button>
                             <button id={tableCSS.save} onClick={updateTransaction}>Save</button>
                         </div>
                     </div>
                 </> :
-                <>
-                    <p>Are you sure you want to delete: {data.name} ({data.type === 'expense' && '-'}${data.amount})?</p>
-                    <div className={deleteCSS.deleteCancelButtons}>
-                        <button id={deleteCSS.cancel} onClick={cancelDelete}>Cancel</button>
-                        <button id={deleteCSS.delete} onClick={(e) => deleteTranscation(e, data.docId)}>Delete</button>
-                    </div>
-                </> 
+                <DeleteTransaction
+                    data={data}
+                    setIsOpen={setIsOpen}
+                    setDeleteOpen={setDeleteOpen}
+                />
             }
         </div>
     );
