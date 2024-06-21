4c367d54839b8a78bd5d8237b423b5087e25817b
Benjamin Pham <97713069+BenPham14@users.noreply.github.com>
2024-06-21 14:58:22 -0700

    Add default profile image for users

--- finance-tracker/src/pages/main/profile/Profile.js
+++ finance-tracker/src/pages/main/profile/Profile.js
@@ -1,46 +1,35 @@
 import { useState } from 'react';
 import mainCSS from '../main.module.css';
-import { IoMdExit } from "react-icons/io";
-import { IoMoon, IoSunny } from "react-icons/io5";
+import { IoPersonCircle } from "react-icons/io5";
+import ProfileMenu from './ProfileMenu.js';
 
 const Profile = ({signOutUser, user, darkMode, setTheme}) => {
     const [profileMenuOpen, setProfileMenuOpen] = useState(false);
 
     return (
         <div className={mainCSS.profile}>
-            <img 
-                onClick={() => setProfileMenuOpen(!profileMenuOpen)} 
-                className={mainCSS.profileImage} 
-                id={profileMenuOpen ? mainCSS.profileOpen : undefined} 
-                src={user.photoURL} 
-                alt='Profile' 
-                referrerPolicy="no-referrer"
-            />
-            {profileMenuOpen &&
-                <div className={mainCSS.profileMenu}>
-                    <div className={mainCSS.profileInfo}>
-                        <img onClick={signOutUser} className={mainCSS.profile} src={user.photoURL} alt='Profile' referrerPolicy="no-referrer"/>
-                        <div>
-                            <p>{user.displayName}</p>
-                            <p>{user.email}</p>
-                        </div>
-                    </div>
-                    {darkMode ? 
-                        <div id={mainCSS.theme} onClick={setTheme}>
-                            <IoSunny/>
-                            <p>Light Mode</p>
-                        </div> :
-                        <div id={mainCSS.theme} onClick={setTheme}>
-                            <IoMoon/>
-                            <p>Dark Mode</p>
-                        </div>
-                    }
-                    <div id={mainCSS.signOut} onClick={signOutUser}>
-                        <IoMdExit/>
-                        <p>Sign Out</p>
-                    </div>
-                </div>
+            {user.photoURL ?
+                <img 
+                    onClick={() => setProfileMenuOpen(!profileMenuOpen)} 
+                    className={mainCSS.profileImage} 
+                    id={profileMenuOpen ? mainCSS.profileOpen : undefined} 
+                    src={user.photoURL} 
+                    alt='Profile' 
+                    referrerPolicy="no-referrer"
+                /> :
+                <IoPersonCircle
+                    onClick={() => setProfileMenuOpen(!profileMenuOpen)}
+                    id={profileMenuOpen ? mainCSS.profileOpen : undefined} 
+                    className={mainCSS.profileImage} 
+                />
             }
+            <ProfileMenu
+                profileMenuOpen={profileMenuOpen}
+                signOutUser={signOutUser}
+                user={user}
+                darkMode={darkMode}
+                setTheme={setTheme}
+            />
         </div>
     );
 };
---
+++ finance-tracker/src/pages/main/profile/ProfileMenu.js
@@ -0,0 +1,42 @@
+import mainCSS from '../main.module.css';
+import { IoMdExit } from "react-icons/io";
+import { IoMoon, IoSunny, IoPersonCircle } from "react-icons/io5";
+
+const ProfileMenu = ({profileMenuOpen, signOutUser, user, darkMode, setTheme}) => {
+    return (
+        <>
+            {profileMenuOpen &&
+                <div className={mainCSS.profileMenu}>
+                    <div className={mainCSS.profileInfo}>
+                        {user.photoURL ?
+                            <img onClick={signOutUser} src={user.photoURL} alt='Profile' referrerPolicy="no-referrer"/> :
+                            <IoPersonCircle
+                                className={mainCSS.defaultProfile}
+                            />
+                        }
+                        <div>
+                            {user.displayName && <p>{user.displayName}</p>}
+                            <p>{user.email}</p>
+                        </div>
+                    </div>
+                    {darkMode ? 
+                        <div id={mainCSS.theme} onClick={setTheme}>
+                            <IoSunny/>
+                            <p>Light Mode</p>
+                        </div> :
+                        <div id={mainCSS.theme} onClick={setTheme}>
+                            <IoMoon/>
+                            <p>Dark Mode</p>
+                        </div>
+                    }
+                    <div id={mainCSS.signOut} onClick={signOutUser}>
+                        <IoMdExit/>
+                        <p>Sign Out</p>
+                    </div>
+                </div>
+            }
+        </>
+    );
+};
+
+export default ProfileMenu;
